c2426499423c5ea79cf667954f618867cc80e877
castroogbs <dev.gbcproject@gmail.com>
2022-12-18 21:21:36 -0300

    API docs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
+// Docs
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+// Routes
 app.use("/users", usersRoutes);
 
 export { app };
